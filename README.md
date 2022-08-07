6739f7c1df25f6d299a9bdbbad8ae13f172b81aa
Бобровский Дмитрий <bob_list@mail.ru>
2022-08-07 16:04:50 +0300

    work on app and telegram notification

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # zabbix-monitoring-programs-execution
-The program controls the execution of other programs and sends the execution result to zabbix, and in case of an execution error, it additionally notifies via telegram.  
+The program controls the execution of programs, scripts or commands OS (any program, scripts and command) and sends the execution result to zabbix, and in case of an execution error, it additionally can notify via telegram.  
 
 ## For developers
-### Get requirements.txt
+### Get and install requirements (requirements.txt)
 `pip freeze | Out-File -Encoding UTF8 c:\MyGit\zabbix-monitoring-programs-execution\requirements.txt`  
 `pip install -r c:\MyGit\zabbix-monitoring-programs-execution\requirements.txt`  
+
+### Telegram
+[sendMessage](https://telegram-bot-sdk.readme.io/reference/sendmessage) `https://api.telegram.org/bot{token}/sendMessage`
+Example message (html):
+```html
+MESSAGE: ❌ Test <b>bold</b>,
+<strong>bold</strong>
+<i>italic</i>, <em>italic</em>
+<a href="URL">inline URL</a>
+<code>inline fixed-width code</code>
+<pre>pre-formatted fixed-width code block</pre>
+```
+
--- zm.py
+++ zm.py
@@ -4,6 +4,7 @@ import os
 import shutil
 import sys
 import traceback
+from urllib import request, parse
 from datetime import datetime
 from pyzabbix import ZabbixAPI
 
@@ -11,48 +12,83 @@ from pyzabbix import ZabbixAPI
 settings = {}
 # Program logger
 logger = logging.getLogger()
+# Executed process
+process = ""
 
 
 def main():
+    global process
     # Get app settings
     try:
         get_settings()
     except Exception as exc:
-        msg = f"ERROR: zm.py cannot get settings\n{exc.returncode}\n{exc.output}"
-        print(msg)
-        telegram_notification(msg)
+        print(f"ERROR: zm.py cannot get settings\n{exc.returncode}\n{exc.output}")
         exit(1)
     # Create app logger
     set_logger()
     logger.debug(settings)
+    # Check required setting
+    if settings.ZM_TELEGRAM_NOTIF and \
+            (not settings.ZM_TELEGRAM_BOT_TOKEN or not settings.ZM_TELEGRAM_CHAT):
+        print(f"ERROR: Telegram notifications are enabled but the parameters ZM_TELEGRAM_BOT_TOKEN, ZM_TELEGRAM_CHAT "
+              f"are not defined\n{exc.returncode}\n{exc.output}")
+        exit(1)
+    if settings.ZM_ZABBIX_SEND and \
+            (not settings.ZM_ZABBIX_URL or not settings.ZM_ZABBIX_USER or not settings.ZM_ZABBIX_PASSWORD):
+        print(f"ERROR: Send data to Zabbix are enabled but the parameters ZM_ZABBIX_URL, ZM_ZABBIX_USER, "
+              f"ZM_ZABBIX_PASSWORD are not defined\n{exc.returncode}\n{exc.output}")
+        exit(1)
     # Check app arguments
     if len(sys.argv) > 2:
         raise_error("Too many arguments", AttributeError)
     elif len(sys.argv) < 2:
         raise_error("Executed process not specified", AttributeError)
     process = sys.argv[1]
-    # Connect to zabbix server
+    # Connect to Zabbix server
     try:
         zapi = ZabbixAPI(url=settings.ZM_ZABBIX_URL, user=settings.ZM_ZABBIX_USER, password=settings.ZM_ZABBIX_PASSWORD)
     except Exception as exc:
-        raise_error("zm.py cannot connect to zabbix", exc)
+        raise_error("zm.py cannot connect to Zabbix", exc)
     # Execute process
     logger.info(f"Start process {process}")
     mine_time = datetime.now()
     execute_cmd(process, message_prefix=message_prefix)
     logger.info(f"Process executed in {datetime.now() - mine_time} sec.")
-    # Send zabbix info
+    # Send Zabbix info
+    if settings.ZM_ZABBIX_SEND:
+        pass
+        # TODO: Send data to Zabbix
 
 
 def raise_error(message, exc):
-    logger.error("ERROR: {message}")
-    logger.error("ERROR:\n", exc.returncode, exc.output)
-    # TODO: telegram message
+    message = f"<b>ERROR</b>: {message}"
+    trace = f"<b>ERROR</b>: <b>returncode</b>={exc.returncode}; <b>output</b>:\n{exc.output}"
+    logger.error(message)
+    logger.error(trace)
+    if settings.ZM_TELEGRAM_NOTIF:
+        message = f"""❌ <b>zm.py</b> from <b>{settings.HOSTNAME}</b>
+Error during process execution <code>{process}</code>
+{message}
+{trace}"""
+        telegram_notification(message)
     exit(1)
 
 
 def telegram_notification(message):
-    pass
+    params = {
+        'chat_id': settings.ZM_TELEGRAM_CHAT,
+        'disable_web_page_preview': '1',
+        'parse_mode': 'HTML',
+        'text': message
+    }
+    logger.debug(params)
+    data = parse.urlencode(params).encode()
+    logger.debug(data)
+    req = request.Request(f"https://api.telegram.org/bot{settings.ZM_TELEGRAM_BOT_TOKEN}/sendMessage", data=data)
+    logger.debug(req)
+    resp = request.urlopen(req)
+    logger.debug(resp)
+    return resp
 
 
 class Settings(object):
@@ -70,19 +106,28 @@ class Settings(object):
 def get_settings():
     global settings
     settings['ZM_DEBUG'] = os.getenv('ZM_DEBUG', False)
+    settings['HOSTNAME'] = os.getenv('HOSTNAME', "Unknown")
     # settings['ZM_LOG_DIR'] = os.getenv('ZM_LOG_DIR', os.path.abspath(os.path.dirname(__file__)))
+    # Should app send telegram alerts? or log messages only to stdout.
+    settings['ZM_TELEGRAM_NOTIF'] = os.getenv('ZM_TELEGRAM_NOTIF', True)
+
     # Zabbix settings
-    # zabbix_sender binary
-    # settings['ZM_ZABBIX_SENDER'] = os.getenv('ZM_ZABBIX_SENDER', "/usr/bin/zabbix_sender")
-    # Zabbix agent configuration file
-    # settings['ZM_ZABBIX_SENDER_CONF'] = os.getenv('ZM_ZABBIX_SENDER_CONF', "/etc/zabbix/zabbix_agentd.conf")
+    # Should app send data to Zabbix?
+    settings['ZM_ZABBIX_SEND'] = os.getenv('ZM_ZABBIX_SEND', True)
     # OK value for Zabbix
     settings['ZM_ZABBIX_OK'] = os.getenv('ZABBIX_OK', 0)
     # Not OK value for Zabbix
     settings['ZM_ZABBIX_NOT_OK'] = os.getenv('ZM_ZABBIX_NOT_OK', 1)
-    settings['ZM_ZABBIX_URL'] = 'http://172.26.12.86'
-    settings['ZM_ZABBIX_USER'] = 'bobrovsky'
-    settings['ZM_ZABBIX_PASSWORD'] = '7#TS9$Vt#Ccu'
+    settings['ZM_ZABBIX_URL'] = os.getenv('ZM_ZABBIX_URL', None)
+    settings['ZM_ZABBIX_USER'] = os.getenv('ZM_ZABBIX_USER', None)
+    settings['ZM_ZABBIX_PASSWORD'] = os.getenv('ZM_ZABBIX_PASSWORD', None)
+
+    # Telegram settings
+    # Telegram connection timeout
+    settings['ZM_TELEGRAM_TIMEOUT'] = os.getenv('ZM_TELEGRAM_TIMEOUT', 10)
+    # Telegram AUTH
+    settings['ZM_TELEGRAM_BOT_TOKEN'] = os.getenv('ZM_TELEGRAM_BOT_TOKEN', None)
+    settings['ZM_TELEGRAM_CHAT'] = os.getenv('ZM_TELEGRAM_CHAT', None)
 
     settings = Settings(settings)
 
@@ -94,7 +139,7 @@ def set_logger():
         logger.setLevel(logging.DEBUG)
     else:
         logger.setLevel(logging.INFO)
-    stdout_log_formatter = logging.Formatter('%(asctime)s|%(levelname)8s| %(message)s')
+    stdout_log_formatter = logging.Formatter('%(asctime)s|%(levelname)-5s|%(funcName)s| %(message)s')
     stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.setFormatter(stdout_log_formatter)
     logger.addHandler(stdout_handler)
@@ -109,15 +154,14 @@ def execute_cmd(cmd, cwd_=None, message=None, message_prefix=None, output_prefix
         logger.info(f"{message_prefix}{message}")
     else:
         logger.info(f"{message_prefix}{cmd}")
-    if not settings.dry_run:
-        try:
-            # output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, )
-            completed_process = subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True, cwd=cwd_)
-        except subprocess.CalledProcessError as exc:
-            raise_error("process ended with error", exc)
-        else:
-            # out = output.decode("utf-8")
-            logger.info(f'{output_prefix}{completed_process.stdout}')
+    try:
+        # output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, )
+        completed_process = subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True, cwd=cwd_)
+    except subprocess.CalledProcessError as exc:
+        raise_error("Process ended with error", exc)
+    else:
+        # out = output.decode("utf-8")
+        logger.info(f'{output_prefix}{completed_process.stdout}')
 
 
 if __name__ == '__main__':
