997bfa832c72d0cc7308cb979a6396351be1b8a1
Бобровский Дмитрий <ministrbob777@gmail.com>
2022-09-15 19:36:10 +0300

    redesign of the program. part 1

--- zm.py
+++ zm.py
@@ -1,36 +1,13 @@
 import os
-import logging
-import os
-import shutil
+import subprocess
 import sys
 import traceback
-import subprocess
-from urllib import request, parse
 from datetime import datetime
-# from pyzabbix import ZabbixAPI
-from pyzabbix import ZabbixMetric, ZabbixSender
 
-# Program settings
-settings = {}
-# Program logger
-logger = logging.getLogger()
-# Executed process
-process = ""
-time_execution = 0
-result = 0
+from zm_lib import raise_error, Settings, zabbix_sender, get_logger
 
 
-def main():
-    global process, time_execution
-    # Get app settings
-    try:
-        get_settings()
-    except Exception:
-        print(f"ERROR: zm.py cannot get settings")
-        exit(1)
-    # Create app logger
-    set_logger()
-    logger.debug(settings)
+def main(settings, logger):
     # Check required setting
     if settings.ZM_TELEGRAM_NOTIF and \
             (not settings.ZM_TELEGRAM_BOT_TOKEN or not settings.ZM_TELEGRAM_CHAT):
@@ -44,85 +21,27 @@ def main():
         exit(1)
     # Check app arguments
     if len(sys.argv) > 2:
-        raise_error("Too many arguments")
+        raise_error(settings, logger, "Too many arguments")
     elif len(sys.argv) < 2:
-        raise_error("Executed process not specified")
+        raise_error(settings, logger, "Executed process not specified")
     process = sys.argv[1]
     # Execute process
     logger.info(f"Start process {process}")
     mine_time = datetime.now()
-    execute_cmd(process)
+    result = execute_cmd(process)
     time_execution = round((datetime.now() - mine_time).total_seconds())
     logger.info(f"Process executed in {time_execution} sec.")
     # Send Zabbix info
     if settings.ZM_ZABBIX_SEND:
         try:
-            zabbix_sender()
+            zabbix_sender(settings, logger, result)
         except Exception:
-            raise_error("zm.py cannot send data to zabbix")
+            raise_error(settings, logger, "zm.py cannot send data to zabbix")
     exit(0)
 
 
-def raise_error(message, do_error_exit=True):
-    logger.error(f"<b>ERROR</b>: {message}")
-    telegram_message = f"""❌ <b>zm.py</b> from <b>{settings.HOSTNAME}</b>
-Error during process execution <code>{process}</code>
-{message}
-"""
-    telegram_notification(telegram_message)
-    if do_error_exit:
-        exit(1)
-
-
-def zabbix_sender():
-    global result
-    metrics = []
-    m = ZabbixMetric(settings.ZM_ZABBIX_HOST_NAME, settings.ZM_ZABBIX_ITEM_NAME, result)
-    metrics.append(m)
-    if settings.ZM_ZABBIX_SEND_TIME:
-        m = ZabbixMetric(settings.ZM_ZABBIX_HOST_NAME, settings.ZM_ZABBIX_ITEM_TIME_NAME, time_execution)
-        metrics.append(m)
-    zbx = ZabbixSender(settings.ZM_ZABBIX_IP)
-    zbx.send(metrics)
-
-
-def telegram_notification(message):
-    if settings.ZM_TELEGRAM_NOTIF:
-        # headers = {
-        #     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)     Chrome/37.0.2049.0 Safari/537.36'
-        # }
-        params = {
-            'chat_id': settings.ZM_TELEGRAM_CHAT,
-            'disable_web_page_preview': '1',
-            'parse_mode': 'HTML',
-            'text': message
-        }
-        logger.debug(f"params={params}")
-        data = parse.urlencode(params).encode()
-        logger.debug(f"data={data}")
-        url = f"https://api.telegram.org/bot{settings.ZM_TELEGRAM_BOT_TOKEN}/sendMessage"
-        logger.debug(f"url={url}")
-        # req = request.Request(url, data=data, method='POST', headers=headers)
-        req = request.Request(url, data=data, method='POST')
-        logger.debug(f"req={req}")
-        resp = request.urlopen(req)
-        logger.debug(f"resp={resp}")
-
-
-class Settings(object):
-    """
-    Program settings like class
-    """
-
-    def __init__(self, iterable=(), **kwargs):
-        self.__dict__.update(iterable, **kwargs)
-
-    def __str__(self):
-        return str(self.__dict__)
-
-
 def get_settings():
-    global settings
+    settings = {}
     # Enable DEBUG mode?
     settings['ZM_DEBUG'] = os.getenv("ZM_DEBUG", 'False').lower() in 'true'
     # For Telegram message to see which host this message is from.
@@ -157,34 +76,43 @@ def get_settings():
     settings['ZM_TELEGRAM_CHAT'] = os.getenv('ZM_TELEGRAM_CHAT', None)
 
     settings = Settings(settings)
-
-
-def set_logger():
-    # Log to stdout
-    global logger
-    if settings.ZM_DEBUG:
-        logger.setLevel(logging.DEBUG)
-    else:
-        logger.setLevel(logging.INFO)
-    stdout_log_formatter = logging.Formatter('%(asctime)s|%(levelname)-5s|%(funcName)s| %(message)s')
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.setFormatter(stdout_log_formatter)
-    logger.addHandler(stdout_handler)
+    return settings
 
 
 def execute_cmd(cmd, cwd_=None):
-    global result
+    result = 0
     try:
         # output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, )
         completed_process = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd_)
     except subprocess.CalledProcessError as exc:
         result = settings.ZM_ZABBIX_NOT_OK
-        raise_error(f"Process ended with error: code={exc.returncode}; error={exc.stderr}", do_error_exit=False)
+        raise_error(settings, logger, f"Process '{cmd}' ended with error: code={exc.returncode}; error={exc.stderr}", do_error_exit=False)
     else:
         result = settings.ZM_ZABBIX_OK
         # out = output.decode("utf-8")
         logger.info(f'{completed_process.stdout}')
+    return result
 
 
 if __name__ == '__main__':
-    main()
+
+    # Get app settings
+    try:
+        settings = get_settings()
+    except:
+        print(f"ERROR: zm.py cannot get settings\n{traceback.format_exc()}")
+        exit(1)
+
+    # Create app logger
+    try:
+        logger = get_logger(settings)
+        logger.debug(settings)
+    except:
+        print(f"ERROR: zm.py cannot get logger\n{traceback.format_exc()}")
+        exit(1)
+
+    # MAIN
+    try:
+        main(settings, logger)
+    except:
+        raise_error(settings, logger, f"{traceback.format_exc()}")
---
+++ zm_lib.py
@@ -0,0 +1,85 @@
+import logging
+import sys
+from urllib import request, parse
+
+from pyzabbix import ZabbixMetric, ZabbixSender
+
+
+class Settings(object):
+    """
+    Program settings like class
+    """
+
+    def __init__(self, iterable=(), **kwargs):
+        self.__dict__.update(iterable, **kwargs)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+
+def get_logger(settings):
+    logger = logging.getLogger()
+    # Log to stdout
+    if settings.ZM_DEBUG:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    stdout_log_formatter = logging.Formatter('%(asctime)s|%(levelname)-5s|%(funcName)s| %(message)s')
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(stdout_log_formatter)
+    logger.addHandler(stdout_handler)
+    return logger
+
+
+def raise_error(settings, logger, message, do_error_exit=True):
+    logger.error(f"<b>ERROR</b>: {message}")
+    telegram_message = f"""❌ <b>zm.py</b> from <b>{settings.HOSTNAME}</b>
+{message}
+"""
+    telegram_notification(settings, logger, telegram_message)
+    if do_error_exit:
+        exit(1)
+
+
+def telegram_notification(settings, logger, message):
+    if settings.ZM_TELEGRAM_NOTIF:
+        # headers = {
+        #     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)     Chrome/37.0.2049.0 Safari/537.36'
+        # }
+        params = {
+            'chat_id': settings.ZM_TELEGRAM_CHAT,
+            'disable_web_page_preview': '1',
+            'parse_mode': 'HTML',
+            'text': message
+        }
+        logger.debug(f"params={params}")
+        data = parse.urlencode(params).encode()
+        logger.debug(f"data={data}")
+        url = f"https://api.telegram.org/bot{settings.ZM_TELEGRAM_BOT_TOKEN}/sendMessage"
+        logger.debug(f"url={url}")
+        # req = request.Request(url, data=data, method='POST', headers=headers)
+        req = request.Request(url, data=data, method='POST')
+        logger.debug(f"req={req}")
+        resp = request.urlopen(req)
+        logger.debug(f"resp={resp}")
+
+
+def zabbix_sender(settings, logger, result):
+    metrics = []
+    logger.info(f"Create metric {settings.ZM_ZABBIX_HOST_NAME} - {settings.ZM_ZABBIX_ITEM_NAME} - {result}")
+    m = ZabbixMetric(settings.ZM_ZABBIX_HOST_NAME, settings.ZM_ZABBIX_ITEM_NAME, result)
+    metrics.append(m)
+    if settings.ZM_ZABBIX_SEND_TIME:
+        m = ZabbixMetric(settings.ZM_ZABBIX_HOST_NAME, settings.ZM_ZABBIX_ITEM_TIME_NAME, time_execution)
+        metrics.append(m)
+    zbx = ZabbixSender(settings.ZM_ZABBIX_IP)
+    logger.info(f"Send metrics")
+    zbx.send(metrics)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
