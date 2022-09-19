87d3dd74d67a493d516e9dba8c538feae26c8968
Бобровский Дмитрий <ministrbob777@gmail.com>
2022-09-19 11:01:45 +0300

    prepare for packaging

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='zmpe',
+      version='1.0.0',
+      description='The program controls the execution of any programs, scripts or commands OS and sends the execution result to zabbix, and in case of an execution error, it additionally can notify via telegram.',
+      packages=['zmpe'],
+      author_email='ministrbob777@gmail.com',
+      zip_safe=False)
