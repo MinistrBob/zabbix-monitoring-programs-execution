281490172f4bf9bedd310381d688596f3acccbe6
Бобровский Дмитрий <ministrbob777@gmail.com>
2022-09-22 14:37:24 +0300

    new publishing

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup
+from zmpe.version import __version__
 
 setup(name='zmpe',
-      version='1.0.1',
+      version=__version__,
       description='The program controls the execution of any programs, scripts or commands OS and sends the execution result to zabbix, and in case of an execution error, it additionally can notify via telegram.',
-      packages=['zmpe'],
+      long_description_content_type="text/markdown",
+      long_description=open('README.md', 'r').read(),
+      url='https://github.com/MinistrBob/zabbix-monitoring-programs-execution.git',
+      author='Dmitry Bobrovsky',
       author_email='ministrbob777@gmail.com',
-      zip_safe=False)
+      packages=['zmpe'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Topic :: System :: Monitoring',
+      ]
+      )
---
+++ zmpe/__init__.py
@@ -0,0 +1,1 @@
+from .zm_lib import Settings, get_logger, raise_error, telegram_notification, zabbix_sender
